6a4bc360710ade2dec3baca9711fd07c15edb921
akiko-b <aokojo@gmail.com>
2018-04-17 13:32:45 +0900

    keras 画像認識サンプル

---
+++ 201804_rasp_ai/keras_ResNet50_sample.py
@@ -0,0 +1,25 @@
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+import sys
+
+args = sys.argv
+
+#学習済みモデルResNet50の読込
+model = ResNet50(weights='imagenet')
+
+#画像判定の為の関数
+def predict(filename, featuresize):
+    img = image.load_img(filename, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    preds = model.predict(preprocess_input(x))
+    results = decode_predictions(preds, top=featuresize)[0]
+    return results
+
+#画像を判定
+#img_path = 'elephant.jpg'
+#results = predict(img_path, 3)
+results = predict(args[1], args[2])
+for result in results:
+    print(result)
